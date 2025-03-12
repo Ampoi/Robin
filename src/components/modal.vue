0275b7b0d261096287115506739de66d3e22d469
Ampoi <tange.toshihiro@outlook.jp>
2025-03-12 22:49:43 +0900

    モーダルの淵で画面を閉じれるようにした

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -1,7 +1,12 @@
 <template>
   <Dialog
-    :open="isOpen" @close="isOpen = false"
-    class="px-16 py-4 bg-stripe bg-cover fixed z-50 top-0 left-0 w-full h-full">
+    :open="isOpen"
+    @close="isOpen = false"
+    as="div"
+    class="px-16 py-4 fixed z-50 top-0 left-0 w-full h-full">
+    <div
+      @click="isOpen = false"
+      class="fixed top-0 left-0 w-full h-full bg-stripe bg-cover -z-10"/>
     <h1 class="font-mono font-semibold fixed top-0 left-1/2 -translate-x-1/2 text-xs text-white bg-black px-2">{{ title }}</h1>
     <DialogPanel
       class="text-white bg-black w-full h-full flex flex-col gap-2 border border-white rounded-2xl p-4">
