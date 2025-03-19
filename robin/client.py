83fbfde4119a274aad28956b7a28e0a31d35b60f
pylotPC <johndoe@example.com>
2025-03-19 13:13:31 +0900

    クライアントを起動できない問題の修正

--- robin/client.py
+++ robin/client.py
@@ -1,18 +1,26 @@
 import rclpy
 import subprocess
+from rclpy.node import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
-process = subprocess.Popen(
-  ["bun", "run", "dev"],
-  stdout=subprocess.PIPE,
-  stderr=subprocess.PIPE,
-  cwd="../client",
-  text=True
-)
+# ROSパッケージ名を取得
+package_name = "robin"
+package_dir = get_package_share_directory(package_name)
+
+client_dir = os.path.normpath(os.path.join(package_dir, "../../../../src", package_name, "client"))
 
 class Client(Node):
   def __init__(self):
     super().__init__('client')
     print("Client Node Initialized")
+    process = subprocess.Popen(
+      ["bun", "run", "dev"],
+      stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+      cwd=client_dir,
+      text=True
+    )
     try:
       for line in process.stdout:
         print(line, end="")
