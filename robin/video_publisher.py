ae07a8018402b55ca67d90ef2a6391bf5324d298
Ampoi <tange.toshihiro@outlook.jp>
2025-03-27 17:39:28 +0900

    🚧多分動く

--- robin/video_publisher.py
+++ robin/video_publisher.py
@@ -11,45 +11,49 @@ import cv2
 import asyncio
 from av import VideoFrame
 from aiortc import VideoStreamTrack
+from cv_bridge import CvBridge
+
 
 def runServer(on_shutdown, offer):
-  app = web.Application()
-  app.on_shutdown.append(on_shutdown)
-  app.router.add_post("/offer", offer)
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_post("/offer", offer)
 
-  cors = aiohttp_cors.setup(
-      app,
-      defaults={
-          "*": aiohttp_cors.ResourceOptions(
-              allow_credentials=True,
-              expose_headers="*",
-              allow_headers="*",
-          )
-      },
-  )
+    cors = aiohttp_cors.setup(
+        app,
+        defaults={
+            "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*",
+            )
+        },
+    )
 
-  for route in list(app.router.routes()):
-      cors.add(route)
+    for route in list(app.router.routes()):
+        cors.add(route)
 
-  web.run_app(app, port=8080)
+    web.run_app(app, port=8080)
+
 
 logging.basicConfig(level=logging.INFO)
 pcs = set()
 
+latest_image_msg = None
+
 class OpenCVCameraStreamTrack(VideoStreamTrack):
     def __init__(self):
         super().__init__()
-        self.cap = cv2.VideoCapture(0)  # カメラ番号 0
 
     async def recv(self):
         pts, time_base = await self.next_timestamp()
 
-        ret, frame = self.cap.read()
-        if not ret:
-            raise RuntimeError("カメラフレームの取得に失敗しました")
+        try:
+          frame = self.bridge.imgmsg_to_cv2(latest_image_msg, desired_encoding='bgr8')
+        except Exception as e:
+          self.get_logger().error(f"Error converting image: {e}")
+          return
 
-        # OpenCVのBGRをRGBへ変換してVideoFrameに変換
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         video_frame = VideoFrame.from_ndarray(frame, format="rgb24")
         video_frame.pts = pts
         video_frame.time_base = time_base
@@ -62,9 +66,7 @@ async def offer(request):
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
-    pc = RTCPeerConnection(configuration=RTCConfiguration(
-      iceServers=[]
-    ))
+    pc = RTCPeerConnection(configuration=RTCConfiguration(iceServers=[]))
     pcs.add(pc)
 
     pc.addTrack(OpenCVCameraStreamTrack())
@@ -80,26 +82,38 @@ async def offer(request):
         ),
     )
 
+
 async def on_shutdown(app):
     coros = [pc.close() for pc in pcs]
     await asyncio.gather(*coros)
     pcs.clear()
 
-class Client(Node):
-  def __init__(self):
-    super().__init__('client')
-    print("Video Publisher Node Initialized")
 
-    runServer(on_shutdown, offer)
+class Client(Node):
+    def __init__(self):
+        super().__init__("client")
+        print("Video Publisher Node Initialized")
+
+        self.image_subscriber = self.create_subscription(
+            Image, IMAGE_SUBSCRIBE_TOPIC_NAME, self.image_callback, 10
+        )
+
+        print("start webrtc server")
+        runServer(on_shutdown, offer)
+
+    def image_callback(self, msg):
+        latest_image_msg = msg
+
 
 def main(args=None):
-  rclpy.init(args=args)
-  
-  client = Client()
-  rclpy.spin(client)
-  
-  client.destroy_node()
-  rclpy.shutdown()
+    rclpy.init(args=args)
 
-if __name__ == '__main__':
-  main()
+    client = Client()
+    rclpy.spin(client)
+
+    client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
