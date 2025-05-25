35c7d7119cee3c22085cf8730c8cd416636358f1
Ampoi <tange.toshihiro@outlook.jp>
2025-05-25 21:02:17 +0900

    Vue Routerのセットアップ

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,4 +2,20 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createRouter, createMemoryHistory } from 'vue-router'
+import Controller from './pages/controller.vue'
+//import Face from './pages/face.vue'
+
+const routes = [
+  { path: '/', component: Controller },
+//  { path: '/face', component: Face },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
